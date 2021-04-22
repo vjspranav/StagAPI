2b91181d946af809d0eb88b6a3124fdb5c5633b2
vjspranav <pranavasri@live.in>
2021-04-23 00:08:47 +0530

    Add logic for getting json

--- routes/downloads.js
+++ routes/downloads.js
@@ -1,9 +1,9 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/:device/:variant', function(req, res, next) {
-  url="download/"+req.params.device + "/" + req.params.variant
+router.get("/:device/:variant", function (req, res, next) {
+  url = "download/" + req.params.device + "/" + req.params.variant;
   res.send(url);
 });
 
